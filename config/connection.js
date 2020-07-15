1d06d9e170351fe5d96457c89653fca26ec55e23
Gary Bergman <garybergman00@gmail.com>
2020-07-15 14:43:35 -0400

    Added Jaws to connect to Heroku

--- config/connection.js
+++ config/connection.js
@@ -2,13 +2,20 @@
 var mysql = require("mysql");
 require("dotenv").config();
 
-var connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: process.env.MYSQL_PASS,
-    database: "burgers_db"
-});
+//Heroku
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+
+    // Local Host
+    var connection = mysql.createConnection({
+        host: "localhost",
+        port: 3306,
+        user: "root",
+        password: process.env.MYSQL_PASS,
+        database: "burgers_db"
+    });
+};
 
 // Make connection.
 connection.connect(function (err) {
